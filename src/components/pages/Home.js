e105cd3ce8c20f980d6d8d70ffe5dc67d7000f3c
knwch <knwch@harmoinc.local>
2020-02-21 01:04:48 +0700

    add hotel card

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import HotelCard from '../cards/HotelCard'
+import { Grid } from 'semantic-ui-react'
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <Grid textAlign='center'>
+                <Grid.Column mobile={16} tablet={6} computer={6}>
+                    <HotelCard />
+                </Grid.Column>
+            </Grid>
         );
     }
 }
