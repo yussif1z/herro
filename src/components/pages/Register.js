10af64b68e9923ce87abd7966d1ef39d20378a7a
zkorra <korrawich.dev@outlook.com>
2020-02-20 14:27:56 +0700

    changed path

---
+++ src/components/pages/Register.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import RegisterForm from '../forms/RegisterForm';
+
+export default class Register extends Component {
+    render() {
+        return (
+            <RegisterForm />
+        );
+    }
+}
