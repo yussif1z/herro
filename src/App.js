9f500e5365f4641a5aa228542319eb14379e64f6
knwch <knwch@harmoinc.local>
2020-02-20 14:31:35 +0700

    create empty home page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          {/* <Route exact path="/" component={Home} /> */}
+          <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
         </Switch>
