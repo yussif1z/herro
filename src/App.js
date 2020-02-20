7e4def38bc01fd6e3787b293caed1e0ab3e725c4
zkorra <korrawich.dev@outlook.com>
2020-02-20 14:10:49 +0700

    created login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Login from './components/pages/Login';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/pages/Login.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import {
+    Responsive,
+    Container,
+    Icon,
+    Input,
+    Button,
+    Form,
+    Grid
+} from 'semantic-ui-react';
+
+export default function Login() {
+    return (
+        <Responsive>
+          <Container fluid>
+            <Grid centered>
+              <Grid.Column mobile={16} tablet={7} computer={6}>
+                <h4 className="text-center mb-4"><div>Sign in</div></h4>
+                <Form>
+  
+                  <Form.Field>
+                    <Input fluid iconPosition='left' placeholder='username'>
+                      <Icon name='user' />
+                      <input type="text" />
+                    </Input>
+                  </Form.Field>
+  
+                  <Form.Field>
+                    <Input fluid iconPosition='left' placeholder='password'>
+                      <Icon name='lock' />
+                      <input type="password" />
+                    </Input>
+                  </Form.Field>
+  
+                  <div>
+                    <Button color='yellow' animated>
+                      <Button.Content visible>Sign in</Button.Content>
+                      <Button.Content hidden>
+                        <Icon name='arrow right' />
+                      </Button.Content>
+                    </Button>
+                  </div>
+  
+                </Form>
+              </Grid.Column>
+            </Grid>
+          </Container>
+        </Responsive>
+      );
+}
