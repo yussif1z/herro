967d0c790f6f7879b6150fc95970fc917a33335a
zkorra <korrawich.dev@outlook.com>
2020-02-21 22:08:41 +0700

    add google map in book

--- src/components/pages/Book.js
+++ src/components/pages/Book.js
@@ -1,34 +1,40 @@
 import React, { Component } from 'react'
 import { Card, Grid } from 'semantic-ui-react'
 import firebase from '../../firebase'
+import MapContainer from '../map/MapContainer'
 
 export default class Book extends Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            hotels: []
+            location: {
+                lat: 13.7563,
+                lng: 100.5018
+            }
         }
-    }
-
-    componentDidMount() {
-
     }
 
     render() {
         return (
-            <Grid className='mb-4' centered>
-                <Grid.Column mobile={15} tablet={5} computer={5}>
+            <Grid centered>
+                <Grid.Column mobile={15} tablet={9} computer={9}>
                     <Card fluid>
-
-                        <Card.Content className='card-color' textAlign='center'>
+                        <Card.Content textAlign='center'>
 
                         </Card.Content>
-
                     </Card>
                 </Grid.Column>
-                <Grid.Column mobile={15} tablet={11} computer={11}>
-                    
+                <Grid.Column mobile={15} tablet={7} computer={7}>
+                    <Card fluid>
+                        <Card.Content textAlign='center'>
+                            <MapContainer 
+                                location={this.state.location} 
+                                markerLatitude={this.state.location.lat}
+                                markerLongitude={this.state.location.lng} 
+                            />
+                        </Card.Content>
+                    </Card>
                 </Grid.Column>
             </Grid>
         );
