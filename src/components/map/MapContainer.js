a69cb7d37d77f7ff9bcc7e3b0a2e09c975eb9e9b
knwch <knwch@harmoinc.local>
2020-02-21 22:08:41 +0700

    add google map in book

---
+++ src/components/map/MapContainer.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import styles from './mapStyles';
+import Marker from './Marker';
+
+class MapContainer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            location: {
+                lat: 13.7563,
+                lng: 100.5018
+            },
+            zoom: 16
+        }
+    };
+
+    render() {
+        return (
+            <div class="map-responsive">
+                <div style={{ height: '50vh', width: '100%' }}>
+                    <GoogleMapReact
+                        bootstrapURLKeys={{
+                            key: 'AIzaSyDxYQoa90Q2clupDUCwMiAAvModGcY2y0k',
+                            language: 'en'
+                        }}
+                        center={this.props.location}
+                        zoom={this.state.zoom}
+                        options={{ styles }}
+                    >
+                        <Marker
+                            lat={this.props.markerLatitude}
+                            lng={this.props.markerLongitude}
+                        >
+                        </Marker>
+                    </GoogleMapReact>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default MapContainer
---
+++ src/components/map/Marker.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
+
+const Marker = (props) => {
+    return (
+        <Icon size='big' name='purple map hotel alternate' />
+    );
+};
+
+export default Marker;
