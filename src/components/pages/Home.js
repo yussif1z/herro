a04932abd3167278a41b3de36c06c1420469048c
zkorra <korrawich.dev@outlook.com>
2020-02-23 01:37:21 +0700

    add picture from storage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, Grid } from 'semantic-ui-react'
+import { Card, Grid, Image } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import firebase from '../../firebase'
 
@@ -20,21 +20,31 @@ export default class HotelCard extends Component {
             .then(querySnapshot => {
                 const hotels = []
                 querySnapshot.forEach(doc => {
-                    hotels.push({
-                        hotelid: doc.id,
-                        name: doc.data().name,
-                        price: doc.data().price,
-                        detail: doc.data().detail,
-                        pictureurl: doc.data().pictureurl,
-                        latitude: doc.data().location.latitude,
-                        longitude: doc.data().location.longitude
-                    })
+                    firebase
+                        .storage()
+                        .refFromURL(doc.data().pictureurl)
+                        .getDownloadURL()
+                        .then(downloadURL => {
+                            hotels.push({
+                                hotelid: doc.id,
+                                name: doc.data().name,
+                                price: doc.data().price,
+                                detail: doc.data().detail,
+                                pictureurl: downloadURL,
+                                latitude: doc.data().location.latitude,
+                                longitude: doc.data().location.longitude
+                            })
+                            this.setState({ hotels })
+                        })
+                        .catch(function (error) {
+                            console.log("Error getting documents: ", error)
+                        })
                 })
-                this.setState({ hotels })
             })
             .catch(function (error) {
                 console.log("Error getting documents: ", error)
             })
+
     }
 
     render() {
@@ -43,28 +53,30 @@ export default class HotelCard extends Component {
                 <Grid.Column mobile={16} tablet={6} computer={6}>
                     {this.state.hotels.map((hotel, index) => {
                         return (
-                            <Link to={`/booking/${hotel.hotelid}`}>
-                                <Card key={index} link className='bg-transparent shadow-none' fluid>
-                                    {/* <Image src={require(hotel.pictureurl)} circular wrapped /> */}
-                                    <Card.Header textAlign='left'>
-                                        <h5>
-                                            {hotel.name}
-                                        </h5>
-                                    </Card.Header>
-                                    <Card.Description textAlign='left'>
-                                        {hotel.detail}
-                                    </Card.Description>
-                                    <Card.Meta textAlign='left'>
-                                        {hotel.price}
-                                    </Card.Meta>
-                                    <Card.Description textAlign='left'>
-                                        {hotel.latitude}
-                                    </Card.Description>
-                                    <Card.Description textAlign='left'>
-                                        {hotel.longitude}
-                                    </Card.Description>
-                                </Card>
-                            </Link>
+                            <div key={index}>
+                                <Link to={`/booking/${hotel.hotelid}`}>
+                                    <Card link className='bg-transparent shadow-none' fluid>
+                                        <Image src={hotel.pictureurl} circular wrapped />
+                                        <Card.Header textAlign='left'>
+                                            <h5>
+                                                {hotel.name}
+                                            </h5>
+                                        </Card.Header>
+                                        <Card.Description textAlign='left'>
+                                            {hotel.detail}
+                                        </Card.Description>
+                                        <Card.Meta textAlign='left'>
+                                            {hotel.price}
+                                        </Card.Meta>
+                                        <Card.Description textAlign='left'>
+                                            {hotel.latitude}
+                                        </Card.Description>
+                                        <Card.Description textAlign='left'>
+                                            {hotel.longitude}
+                                        </Card.Description>
+                                    </Card>
+                                </Link>
+                            </div>
                         );
                     })}
                 </Grid.Column>
