5cda8bdb5a4a6bb913b1f3d22fe0198988fccc66
zkorra <korrawich.dev@outlook.com>
2020-02-23 16:54:05 +0700

    adjust menu ui

--- src/components/menu/MenuBar.js
+++ src/components/menu/MenuBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Menu, Image, Header } from 'semantic-ui-react'
+import { Button, Menu, Image, Header, Dropdown, Icon } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import firebase from '../../firebase'
 
@@ -8,16 +8,24 @@ export default class MenuBar extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            isAuth: false
+            isAuth: false,
+            name: ''
         }
     }
 
     componentDidMount() {
+        const db = firebase.firestore()
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
-                this.setState({
-                    isAuth: true
-                })
+                db.collection('users')
+                    .doc(user.uid)
+                    .get()
+                    .then(doc => {
+                        this.setState({
+                            isAuth: true,
+                            name: doc.data().firstname
+                        })
+                    })
             } else if (!user) {
                 this.setState({
                     isAuth: false
@@ -47,15 +55,22 @@ export default class MenuBar extends Component {
                     <Menu.Item href='/' link>
                         <Header size='small'>Discover</Header>
                     </Menu.Item>
+                    <Menu.Item href='/mybooking' link>
+                        <Header size='small'>My Booking</Header>
+                    </Menu.Item>
                     <Menu.Menu position='right'>
-                        <Menu.Item>
-                            <Link to='/mybooking'>
-                                <Button>My Booking</Button>
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Item onClick={this.logout} link>
-                            <Header size='small'>Logout</Header>
-                        </Menu.Item>
+                        <Dropdown
+                            className='icon'
+                            text={
+                                <Header size='small'>{`${this.state.name}` + `'s`}</Header>
+                            }
+                            item
+                            header
+                        >
+                            <Dropdown.Menu>
+                                <Dropdown.Item icon='sign-out' text='Sign out' onClick={this.logout} />
+                            </Dropdown.Menu>
+                        </Dropdown>
                     </Menu.Menu>
                 </Menu>
             );
